0ced2f421cd759c39b3ced7dd70466bba28f32ee
Jinish9302 <87082055+Jinish9302@users.noreply.github.com>
2023-05-19 03:43:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -44,4 +44,4 @@ def serve_page(name = None):
         return render_template('index.html', variable = ret['matches'])
     return render_template('index.html', variable = [])
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(debug=False, host = '0.0.0.0')
